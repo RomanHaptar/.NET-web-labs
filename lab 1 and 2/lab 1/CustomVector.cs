20d4ec2820b20bd57260239b8d898d7a93b3b11f
RomanHaptar <gaptar4953@ukr.net>
2022-06-02 10:44:47 +0300

    lab 1 and 2

---
+++ lab 1 and 2/lab 1/CustomVector.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomCollection
+{
+    public class CustomVector<T> : IList<T>, IEnumerable<T>
+    {
+        private T[] _vector;
+
+        public int Count { get; protected set; } = 0;
+        public bool IsReadOnly => false;
+        public int Capacity { get => _vector.Length; }
+
+        public CustomVector()
+        {
+            _vector = new T[2];
+        }
+
+        public CustomVector(int capacity)
+        {
+            if (capacity < 1) capacity = 1;
+            _vector = new T[capacity];
+        }
+
+        public CustomVector(IEnumerable<T> collection) : this()
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException(nameof(index));
+                }
+                return _vector[index];
+            }
+            set
+            {
+                if (IsReadOnly == false)
+                {
+                    if (index < 0 || index >= Count)
+                    {
+                        throw new IndexOutOfRangeException(nameof(index));
+                    }
+                    _vector[index] = value;
+                }
+                throw new InvalidOperationException("This array is ReadOnly.");
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (!HasFreeCells()) IncreaseSize();
+            _vector[Count] = item;
+            Count++;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        public void Clear()
+        {
+            _vector = new T[1];
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) > -1;
+        }
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+            if (index > -1)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                _vector[i] = _vector[i + 1];
+            }
+            Count--;
+            _vector[Count] = default(T);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+
+            T[] newArray;
+
+            if (Capacity == Count)
+            {
+                newArray = new T[2 * Capacity];
+            }
+            else
+            {
+                newArray = new T[Capacity];
+            }
+
+            Array.Copy(_vector, 0, newArray, 0, index);
+            newArray[index] = item;
+            Array.Copy(_vector, index, newArray, index + 1, Count - index);
+
+            _vector = newArray;
+            Count++;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(_vector[i], item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            if (arrayIndex < 0)
+            {
+                throw new IndexOutOfRangeException(nameof(arrayIndex));
+            }
+            if (array.Length - arrayIndex < Count)
+            {
+                throw new ArgumentException(nameof(arrayIndex));
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                array[arrayIndex] = _vector[i];
+                arrayIndex++;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+
+            Array.Copy(_vector, 0, array, 0, Count);
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _vector[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private bool HasFreeCells()
+        {
+            return Count < Capacity;
+        }
+
+        private void IncreaseSize()
+        {
+            var newVector = new T[Capacity * 2];
+            for (int i = 0; i < Capacity; i++)
+            {
+                newVector[i] = _vector[i];
+            }
+            _vector = newVector;
+        }
+    }
+}
---
+++ lab 1 and 2/lab 1Tests/CustomVectorTests.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomCollection;
+using System;
+using System.Collections.Generic;
+
+namespace CustomCollection.Tests
+{
+    [TestClass()]
+    public class CustomVectorTests
+    {
+        [TestMethod()]
+        public void CustomVectorTest()
+        {
+            CustomVector<int> vector = new CustomVector<int>();
+            Assert.IsTrue(vector.Count == 0);
+
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            vector = new CustomVector<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+            Assert.IsTrue(vector.Count == list.Count);
+            Assert.IsTrue(list.Capacity == vector.Capacity);
+
+
+            int capacity = 50;
+            vector = new CustomVector<int>(capacity);
+            list = new List<int>(capacity);
+            Assert.IsTrue(vector.Capacity == list.Capacity);
+            Assert.IsTrue(vector.Count == list.Count);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!Equals(vector[i], list[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            List<int> check = new List<int>() { 2, 6, 8, 5, 5, 1, 8, 5, 3, 5 };
+
+            CustomVector<int> vector = new CustomVector<int>();
+            vector.Add(2);
+            vector.Add(6);
+            vector.Add(8);
+            vector.Add(5);
+            vector.Add(5);
+            vector.Add(1);
+            vector.Add(8);
+            vector.Add(5);
+            vector.Add(3);
+            vector.Add(5);
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            CustomVector<int> vector = new CustomVector<int>() { 2, 8, 5, 1, 8, 50, 5, 60, 5 };
+            List<int> check = new List<int>() { 2, 8, 5, 1, 8, 50, 5, 60, 5 };
+
+            vector.Clear();
+            check.Clear();
+            Assert.AreEqual(0, vector.Count);
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            CustomVector<int> vector = new CustomVector<int>() { 2, 8, 5, 1, 8, 50, 5, 60, 5 };
+            Assert.IsTrue(vector.Contains(1));
+
+            Assert.IsTrue(vector.Contains(2));
+
+            Assert.IsFalse(vector.Contains(4));
+
+            vector.Add(4);
+            Assert.IsTrue(vector.Contains(4));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            List<int> check = new List<int>() { 2, 8, 5, 5, 1, 8, 5, 5 };
+            CustomVector<int> vector = new CustomVector<int>() { 2, 6, 8, 5, 5, 1, 8, 5, 3, 5 };
+            vector.Remove(3);
+            vector.Remove(7);
+            vector.Remove(6);
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveAtTest()
+        {
+            List<int> check = new List<int>() { 2, 8, 5, 1, 8, 50, 5, 60, 5 };
+            CustomVector<int> vector = new CustomVector<int>() { 0, 2, 8, 5, 5, 1, 8, 50, 5, 60, 5, 70 };
+            vector.RemoveAt(4);
+            vector.RemoveAt(0);
+            vector.RemoveAt(vector.Count - 1);
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveAtTestFail()
+        {
+            CustomVector<int> vector = new CustomVector<int>() { 2, 8, 5, 1, 8, 50, 5, 60, 5 };
+            try
+            {
+                vector.RemoveAt(vector.Count);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException) { }
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            List<int> check = new List<int>() { 0, 2, 8, 5, 5, 1, 8, 50, 5, 60, 5, 70 };
+            CustomVector<int> vector = new CustomVector<int>() { 2, 8, 5, 5, 1, 8, 5, 5 };
+            vector.Insert(6, 50);
+            vector.Insert(0, 0);
+            vector.Insert(vector.Count - 1, 60);
+            vector.Insert(vector.Count, 70);
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+        [TestMethod()]
+        public void InsertTestFail()
+        {
+            CustomVector<int> vector = new CustomVector<int>() { 0, 2, 8, 5, 5, 1, 8, 50, 5, 60, 5, 70 };
+            try
+            {
+                vector.Insert(vector.Count + 1, -1);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException) { }
+        }
+        [TestMethod()]
+        public void IndexOfTest()
+        {
+            List<string> check = new List<string>() { "A", "AB", "ABC", "ABCD", "ABCDE", null, "ABCDEF", "ABCDEFG", " ", "ABCDEFGH", "ABCDEFGHI" };
+            CustomVector<string> vector = new CustomVector<string>() { "A", "AB", "ABC", "ABCD", "ABCDE", null, "ABCDEF", "ABCDEFG", " ", "ABCDEFGH", "ABCDEFGHI" };
+
+            for (int i = 0; i < check.Count; i++)
+            {
+                Assert.IsTrue(vector.IndexOf(check[i]) == i);
+            }
+
+            Assert.IsTrue(vector.IndexOf(" ") == check.IndexOf(" "));
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            CustomVector<string> vector = new CustomVector<string>() { "A", "AB", "ABC", "ABCDE", null, "ABCDEF", "ABCDEFG", " ", "ABCDEFGHI" };
+            string[] check = new string[vector.Count];
+            vector.CopyTo(check, 0);
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            List<string> check = new List<string>() { "", "hjchy", "ek", "32", null, "%$", "(PL::L" };
+            CustomVector<string> vector = new CustomVector<string>() { "", "hjchy", "ek", "32", null, "%$", "(PL::L" };
+
+            int index = 0;
+
+            foreach (var item in vector)
+            {
+                Assert.AreEqual(item, check[index++]);
+            }
+        }
+
+        [TestMethod()]
+        public void AddRangeTest()
+        {
+            List<string> check = new List<string>() { "", null, "1" };
+            CustomVector<string> vector = new CustomVector<string>() { "", null, "1" };
+
+            vector.AddRange(new string[] { "AB", "dh", "ef" });
+            check.AddRange(new string[] { "AB", "dh", "ef" });
+
+            for (int i = 0; i < check.Count; i++)
+            {
+                if (!Equals(vector[i], check[i]))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void ToArrayTest()
+        {
+            List<string> check = new List<string>() { "A", "AB", "ABC", "ABCD", "ABCDE", "ABCDEF", "ABCDEFG", " ", "ABCDEFGH", "ABCDEFGHI" };
+            CustomVector<string> vector = new CustomVector<string>() { "A", "AB", "ABC", "ABCD", "ABCDE", "ABCDEF", "ABCDEFG", " ", "ABCDEFGH", "ABCDEFGHI" };
+
+            var vectorArray = vector.ToArray();
+            var checkArray = check.ToArray();
+
+            for (int i = 0; i < checkArray.Length; i++)
+            {
+                if (!Equals(vectorArray[i], checkArray[i]))
+                    Assert.Fail();
+            }
+
+            Assert.IsTrue(vectorArray.Length == checkArray.Length);
+        }
+
+    }
+}
